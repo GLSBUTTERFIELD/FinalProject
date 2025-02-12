891dc50f0eb71c12b77f5511b995889b0b709fe0
Reiu-arch <vidaldominguez60@gmail.com>
2025-02-12 13:23:39 -0700

    entity user and test completed

--- RoundTwoJPA/src/main/java/com/skilldistillery/roundtwo/entities/User.java
+++ RoundTwoJPA/src/main/java/com/skilldistillery/roundtwo/entities/User.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.roundtwo.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,11 @@ public class User {
 	private String role;
 	private boolean enabled;
 	
+	
+	public User() {
+		super();
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -56,5 +63,39 @@ public class User {
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + ", enabled="
+				+ enabled + "]";
+	}
+
+	public User(int id, String username, String password, String role, boolean enabled) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.enabled = enabled;
+	}
+	
+	
 	
 }
