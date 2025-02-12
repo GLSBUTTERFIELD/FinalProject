93413d5cedb29d41824e627225ac1d077c84acb8
RaymondCostales <raymond.w.costales@gmail.com>
2025-02-12 14:15:23 -0700

    Create angular application, full stack smoke test

---
+++ ngRoundTwo/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ngRoundTwo';
+
+
+
+  constructor(
+    private auth: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('test','test').subscribe({ // change username to match DB
+      next: (data) => {
+        console.log('Logged in:');
+        console.log(data);
+      },
+      error: (fail) => {
+        console.error('Error authenticating:')
+        console.error(fail);
+      }
+    });
+  }
+
+
+
+}
