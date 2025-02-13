76ea80eaa409ef04a8e15e158e05d3389b7512cd
Reiu-arch <vidaldominguez60@gmail.com>
2025-02-13 12:12:53 -0700

    ready to merge inventory item branch

---
+++ RoundTwoJPA/src/main/java/com/skilldistillery/roundtwo/entities/InventoryItem.java
@@ -0,0 +1,149 @@
+package com.skilldistillery.roundtwo.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inventory_item")
+public class InventoryItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String notes;
+	
+	private boolean avaliable;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+	
+	private boolean enabled;
+	
+	@CreationTimestamp
+	@Column(name = "create_date")
+	private LocalDateTime createDate;
+	
+	@UpdateTimestamp
+	@Column(name = "last_update")
+	private LocalDateTime lastUpdate;
+
+	public InventoryItem() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public boolean isAvaliable() {
+		return avaliable;
+	}
+
+	public void setAvaliable(boolean avaliable) {
+		this.avaliable = avaliable;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InventoryItem other = (InventoryItem) obj;
+		return id == other.id;
+	}
+
+	public InventoryItem(int id, String name, String notes, boolean avaliable, String imageUrl, boolean enabled,
+			LocalDateTime createDate, LocalDateTime lastUpdate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.notes = notes;
+		this.avaliable = avaliable;
+		this.imageUrl = imageUrl;
+		this.enabled = enabled;
+		this.createDate = createDate;
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public String toString() {
+		return "InventoryItem [id=" + id + ", name=" + name + ", notes=" + notes + ", avaliable=" + avaliable
+				+ ", imageUrl=" + imageUrl + ", enabled=" + enabled + ", createDate=" + createDate + ", lastUpdate="
+				+ lastUpdate + "]";
+	}
+	
+
+}
