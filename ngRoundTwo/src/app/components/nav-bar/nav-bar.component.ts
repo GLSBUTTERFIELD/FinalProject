183d983be5a996cf64cf5be1c6bf8d086dc1b721
Reiu-arch <vidaldominguez60@gmail.com>
2025-02-14 15:57:03 -0700

    signup and login completed

--- ngRoundTwo/src/app/components/nav-bar/nav-bar.component.ts
+++ ngRoundTwo/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,9 @@
+import { AuthService } from './../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit } from '@angular/core';
 declare var $: any;
 import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-nav-bar',
@@ -17,19 +19,79 @@ export class NavBarComponent {
 
   showSignUpForm: boolean = false;
 
+  showLoginForm: boolean = false;
+
+  newUser: User = new User();
+
+  loggingInUser: User = new User();
+
+  constructor(
+    private authService: AuthService
+  ){
+  }
+  register(addingUser: User){
+    console.log(addingUser);
+    if(addingUser){
+      addingUser.enabled = true;
+      this.authService.register(addingUser).subscribe({
+
+        next: (registeredUser) => {
+          this.authService.login(addingUser.username, addingUser.password).subscribe({
+            next: (loggedInUser) => {
+              // this.router.navigateByUrl('/todo');
+            },
+            error: (problem) => {
+              console.error('NavBarComponent.register(): Error logging in user:');
+              console.error(problem);
+            }
+          });
+        },
+        error: (fail) => {
+          console.error('NavBarComponent.register(): Error registering account');
+          console.error(fail);
+        }
+
+      });
+
+    }
+  }
+  login(loginUser: User){
+    console.log(loginUser.username + " "+loginUser.password);
+    this.authService.login(loginUser.username, loginUser.password).subscribe({
+
+      next: (displayingUser) => {
+        // this.router.navigateByUrl('/todo');
+        console.log(displayingUser);
+      }, error: (err) => {
+        console.log(err);
+      }
+
+  })
+}
+
+
+
   toggleSignUp() {
     this.showSignUpForm = !this.showSignUpForm;
+  }
+  
+  toggleLogin(){
+    this.showLoginForm = !this.showLoginForm;
   }
 
   ngAfterViewInit() {
     $('#signUpModal').modal({ show: false });
+    $('#loginModal').modal({ show: false });
   }
 
   openModal() {
     $('#signUpModal').modal('show');
+    $('#loginModal').modal('show');
   }
 
   closeModal() {
     $('#signUpModal').modal('hide');
+    $('#loginModal').modal('hide');
   }
+
 }
--- ngRoundTwo/src/app/models/user.ts
+++ ngRoundTwo/src/app/models/user.ts
@@ -4,19 +4,35 @@ export class User {
   username: string;
   password: string;
   enabled: boolean;
+  firstName: string;
+  lastName: string;
   role: string;
+  email: string;
+  imageUrl: string;
+  biography: string;
+
 
   constructor(
     id: number = 0 ,
     username: string = '',
     password: string = '',
     enabled: boolean = false,
-    role: string = ''
+    firstName: string = '',
+    lastName: string = '',
+    role: string = '',
+    email: string = '',
+    imageUrl: string = '',
+    biography: string = ''
   ){
     this.id = id;
     this.username = username;
     this.password = password;
     this.enabled = enabled;
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.role  = role;
+    this.email = email;
+    this.imageUrl = imageUrl;
+    this.biography = biography;
   }
 }
