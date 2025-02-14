2634b93bef81543532372d6fdfc343eb27f06c62
William Brown <wsbrown.colo@gmail.com>
2025-02-14 12:01:47 -0700

    InventoryItem relations, Junit test passed

--- RoundTwoJPA/src/main/java/com/skilldistillery/roundtwo/entities/InventoryItemComment.java
+++ RoundTwoJPA/src/main/java/com/skilldistillery/roundtwo/entities/InventoryItemComment.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.roundtwo.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -11,6 +12,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,9 +31,28 @@ public class InventoryItemComment {
 	@UpdateTimestamp
 	@Column(name = "last_update")
 	private LocalDateTime lastUpdate;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "inventory_item_id")
+	private InventoryItem inventoryItem;
+	
+	@ManyToOne
+	@JoinColumn(name = "in_reply_to_id")
+	private InventoryItemComment parentComment;
+	
+	@OneToMany(mappedBy = "parentComment")
+	private List<InventoryItemComment> subComment;
+	
+	
 
 	public InventoryItemComment() {
 	}
+	
+	
 
 	public int getId() {
 		return id;
@@ -70,6 +93,33 @@ public class InventoryItemComment {
 	public void setLastUpdate(LocalDateTime lastUpdate) {
 		this.lastUpdate = lastUpdate;
 	}
+
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public InventoryItem getInventoryItem() {
+		return inventoryItem;
+	}
+	public void setInventoryItem(InventoryItem inventoryItem) {
+		this.inventoryItem = inventoryItem;
+	}
+	public InventoryItemComment getParentComment() {
+		return parentComment;
+	}
+	public void setParentComment(InventoryItemComment parentComment) {
+		this.parentComment = parentComment;
+	}
+	public List<InventoryItemComment> getSubComment() {
+		return subComment;
+	}
+	public void setSubComment(List<InventoryItemComment> subComment) {
+		this.subComment = subComment;
+	}
+
+
 
 	@Override
 	public int hashCode() {
