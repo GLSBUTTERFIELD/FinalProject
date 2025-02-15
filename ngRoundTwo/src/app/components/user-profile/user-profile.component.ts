ce2e4943bbca1a00ed85faca56f35da2ba2c706c
William Brown <wsbrown.colo@gmail.com>
2025-02-14 17:33:15 -0700

    showMethod in userService

--- ngRoundTwo/src/app/components/user-profile/user-profile.component.ts
+++ ngRoundTwo/src/app/components/user-profile/user-profile.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,8 +16,25 @@ import { UserService } from '../../services/user.service';
 })
 export class UserProfileComponent {
 
+  selected: User | null = null;
+
   constructor(
     private userService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ){}
+
+  loadProfileById(userId: number) {
+    this.userService.show(userId).subscribe({
+      next: (user) => {
+      this.selected = user;
+    },
+      error: (err) => {
+        this.router.navigateByUrl('User ' + userId + ' not found');
+        console.error(err);
+        console.error("Error loading User in userProfile component");
+      }
+    });
+  }
 
 }
--- ngRoundTwo/src/app/services/user.service.ts
+++ ngRoundTwo/src/app/services/user.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable, catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private url = environment.baseUrl + 'api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  public show(userId: number) : Observable<User> {
+    return this.http.get<User>(this.url + "/" + userId, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('UserService.show(): error finding specified UserId: ' + err)
+        );
+      })
+    );
+  }
+
 }
