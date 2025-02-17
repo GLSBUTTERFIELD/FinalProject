23fda2dd36482b1f1f0e7b932559b7bd2e81e6e3
William Brown <wsbrown.colo@gmail.com>
2025-02-17 14:48:41 -0700

    gatheringComment model stub out

---
+++ ngRoundTwo/src/app/models/gathering-comment.spec.ts
@@ -0,0 +1,7 @@
+import { GatheringComment } from './gathering-comment';
+
+describe('GatheringComment', () => {
+  it('should create an instance', () => {
+    expect(new GatheringComment()).toBeTruthy();
+  });
+});
