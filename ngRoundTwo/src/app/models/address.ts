0c13f1fef3fa1562a86058cbcd8a88ab5116173e
Reiu-arch <vidaldominguez60@gmail.com>
2025-02-17 14:11:37 -0700

    address model stubbed out

--- ngRoundTwo/src/app/models/address.ts
+++ ngRoundTwo/src/app/models/address.ts
@@ -1,2 +1,27 @@
+
+
 export class Address {
+  id: number;
+  name: string;
+  street: string;
+  stateAbbr: string;
+  zip: string;
+  city: string;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    street: string = '',
+    stateAbbr: string = '',
+    zip: string = '',
+    city: string = ''
+  ){
+    this.id = id;
+    this.name = name;
+    this.street = street;
+    this.stateAbbr = stateAbbr;
+    this.zip = zip;
+    this.city = city;
+  }
+
 }
