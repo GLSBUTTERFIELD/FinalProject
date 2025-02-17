ddd854fa0f31e133a3f4950cdf5c0900b82282b4
Gabby <glsbutterfield@gmail.com>
2025-02-17 15:12:34 -0700

    stubbed out Game component html, game service, game model, category model

--- ngRoundTwo/src/app/app.routes.ts
+++ ngRoundTwo/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { GatheringComponent } from './components/gathering/gathering.component';
 import { TradeComponent } from './components/trade/trade.component';
+import { GameComponent } from './components/game/game.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo:'home'},
   { path: 'home', component: HomeComponent},
   { path: 'about', component: AboutComponent},
   {path: 'gathering', component: GatheringComponent},
+  {path: 'games', component: GameComponent},
   {path: 'gathering/:gatheringId', component: GatheringComponent},
   {path: 'trade', component: TradeComponent},
   {path: 'userprofile/:userId', component: UserProfileComponent},
