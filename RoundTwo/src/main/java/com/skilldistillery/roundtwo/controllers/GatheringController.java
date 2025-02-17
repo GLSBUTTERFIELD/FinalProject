1acb6806be9872e9aa868ca97258fc116af7d543
Gabby <glsbutterfield@gmail.com>
2025-02-17 16:08:11 -0700

    stub out Create Gathering and test in postman

--- RoundTwo/src/main/java/com/skilldistillery/roundtwo/controllers/GatheringController.java
+++ RoundTwo/src/main/java/com/skilldistillery/roundtwo/controllers/GatheringController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,25 +19,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
-@CrossOrigin({"*", "http://localhost/"})
+@CrossOrigin({ "*", "http://localhost/" })
 @RequestMapping("api")
 public class GatheringController {
 
 	@Autowired
 	private GatheringService gatheringService;
-	
-	
+
 	// --------------------------------------------------------------------------------------\\
 
 //  GET ALL
 	@GetMapping("gatherings")
 	public List<Gathering> index() {
-		return gatheringService.index(); 
+		return gatheringService.index();
 	}
-	
+
 //  GET by Id
 	@GetMapping("gatherings/{gatheringId}")
-	public Gathering showGathering (@PathVariable("gatheringId") int gatheringId, HttpServletRequest req, HttpServletResponse res) {
+	public Gathering showGathering(@PathVariable("gatheringId") int gatheringId, HttpServletRequest req,
+			HttpServletResponse res) {
 		Gathering foundGathering = null;
 		try {
 			foundGathering = gatheringService.findById(gatheringId);
@@ -49,6 +51,26 @@ public class GatheringController {
 		}
 		return foundGathering;
 	}
-	
-	
+
+//POST new gathering
+	@PostMapping("gatherings")
+	public Gathering add(@RequestBody Gathering gathering, HttpServletResponse res, HttpServletRequest req,
+			Principal principal) {
+		Gathering createdGathering = null;
+		try {
+			createdGathering = gatheringService.create(principal.getName(), gathering);
+			if (createdGathering == null) {
+				res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			} else {
+				res.setStatus(HttpServletResponse.SC_CREATED);
+				res.setHeader("Location", req.getRequestURL().append('/').append(createdGathering.getId()).toString());
+			}
+		} catch (Exception e) {
+			res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			createdGathering = null;
+			e.printStackTrace();
+		}
+		return createdGathering;
+	}
+
 }
